34ecca83a93294bf8d34eb31f3fb8422c12b61aa
李纺 <2474061008@qq.com>
2023-08-12 17:50:42 +0800

    Update draw_rate.py

--- multi-model_application/modules/draw_rate.py
+++ multi-model_application/modules/draw_rate.py
@@ -1,41 +1,61 @@
 import matplotlib.pyplot as plt
 from datetime import datetime
+import numpy as np
 
-# 读取数据
+# Read data
 def read_data(file_path):
     times = []
     rates = []
     with open(file_path, 'r') as f:
         for line in f:
-            if line.startswith('INFO'):
-                parts = line.strip().split()
-                time_str = parts[1] + ' ' + parts[2]
-                rate = float(parts[-1])
-                time = datetime.strptime(time_str, '%Y-%m-%d %H:%M:%S,%f')
-                times.append(time)
-                rates.append(rate)
+            #print(line)
+            parts = line.split(" - ")
+            time_part = parts[0]
+            time = datetime.strptime(time_part, '%Y-%m-%d %H:%M:%S,%f')
+            rate_part = parts[-1]
+            rate = float(rate_part)
+            # print(time_part, rate_part)
+
+            times.append(time)
+            rates.append(rate)
     return times, rates
 
-# 读取数据并作图
-def plot_data(file_paths, labels):
-    plt.figure(figsize=(10, 6))
+# Plot data
+def plot_data(ax, times, rates, label):
+    # Calculate mean and standard deviation
+    average_rate = np.mean(rates)
+    std_rate = np.std(rates)
 
-    for i, file_path in enumerate(file_paths):
-        times, rates = read_data(file_path)
-        plt.plot(times, rates, label=labels[i])
+    # Remove points with large deviations from the mean
+    filtered_times = []
+    filtered_rates = []
+    for time, rate in zip(times, rates):
+        if abs(rate - average_rate) <= 3 * std_rate:  # Adjust threshold or multiplier
+            filtered_times.append(time)
+            filtered_rates.append(rate)
 
-    plt.xlabel('Time')
-    plt.ylabel('Rate')
-    plt.title('Rate over Time')
-    plt.legend()
-    plt.xticks(rotation=45)
-    plt.tight_layout()
+    ax.plot(filtered_times, filtered_rates, label=label)
 
-    plt.show()
-
-# 文件路径和标签
+# File paths and labels
 file_paths = ['logs_rate/model_1_rate.log', 'logs_rate/model_2_rate.log', 'logs_rate/model_3_rate.log', 'logs_rate/model_4_rate.log', 'logs_rate/model_5_rate.log']
 labels = ['Model 1', 'Model 2', 'Model 3', 'Model 4', 'Model 5']
 
-# 绘制图像
-plot_data(file_paths, labels)
+# Create subplots
+fig, axs = plt.subplots(5, 1, figsize=(10, 30), sharex=True)
+
+# Plot each subplot
+for i, (ax, file_path, label) in enumerate(zip(axs, file_paths, labels)):
+    times, rates = read_data(file_path)
+    plot_data(ax, times, rates, label)
+    ax.set_ylabel('Rate')
+    ax.set_title(label)
+
+# Set common x-axis label
+axs[-1].set_xlabel('Time')
+
+# Adjust layout
+plt.xticks(rotation=45)
+plt.tight_layout()
+
+# Show the plot
+plt.show()
